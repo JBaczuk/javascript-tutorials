4a4f248ba90d3ffc74c48f806759983cf574941c
Jordan Baczuk <jordan.baczuk@gmail.com>
2021-11-13 14:48:38 -0700

    add mocks to jest

--- jest/temperature.js
+++ jest/temperature.js
@@ -1,6 +1,24 @@
-module.exports.fahrenheitToCelsius = (degrees) => {
+const roundDecimals = (value) => {
+  return Math.round((value + Number.EPSILON) * 100) / 100
+}
+
+const fahrenheitToCelsius = (degrees) => {
   if (typeof degrees !== 'number') throw new TypeError()
   if (degrees < -459.67) throw RangeError()
-  const conversion = (degrees - 32) * (5 / 9)
-  return Math.round((conversion + Number.EPSILON) * 100) / 100
+  return (degrees - 32) * (5 / 9)
 }
+
+const convertDegrees = (degrees) => {
+  // we need to reference the module instance so jest can mock the entire module
+  return temperatureHelpers.roundDecimals(
+    temperatureHelpers.fahrenheitToCelsius(degrees)
+  )
+}
+
+const temperatureHelpers = {
+  convertDegrees,
+  fahrenheitToCelsius,
+  roundDecimals
+}
+
+module.exports = temperatureHelpers
--- jest/temperature.spec.js
+++ jest/temperature.spec.js
@@ -1,11 +1,19 @@
-const { fahrenheitToCelsius } = require('./temperature')
+const temperatureFuncs = require('./temperature')
+const { roundDecimals, fahrenheitToCelsius, convertDegrees } = temperatureFuncs
+
+describe('roundDecimals', () => {
+  it('rounds to nearest 2 decimals', () => {
+    expect(roundDecimals(12.345)).toBe(12.35)
+    expect(roundDecimals(12.344)).toBe(12.34)
+  })
+})
 
 describe('fahrenheitToCelsius', () => {
   it('converts 0', () => {
-    expect(fahrenheitToCelsius(0)).toBe(-17.78)
+    expect(fahrenheitToCelsius(0)).toBe(-17.77777777777778)
   })
   it('converts minimum temperature (−273.15C or -459.67F)', () => {
-    expect(fahrenheitToCelsius(-459.67)).toBe(-273.15)
+    expect(fahrenheitToCelsius(-459.67)).toBe(-273.15000000000003)
   })
   it('converts freezing', () => {
     expect(fahrenheitToCelsius(32)).toBe(0)
@@ -15,9 +23,9 @@ describe('fahrenheitToCelsius', () => {
   })
   it('converts various temperatures', () => {
     expect(fahrenheitToCelsius(-40)).toBe(-40)
-    expect(fahrenheitToCelsius(-20)).toBe(-28.89)
-    expect(fahrenheitToCelsius(10)).toBe(-12.22)
-    expect(fahrenheitToCelsius(75)).toBe(23.89)
+    expect(fahrenheitToCelsius(-20)).toBe(-28.88888888888889)
+    expect(fahrenheitToCelsius(10)).toBe(-12.222222222222223)
+    expect(fahrenheitToCelsius(75)).toBe(23.88888888888889)
   })
   it('throws if below range', () => {
     expect(() => fahrenheitToCelsius(-459.68)).toThrow()
@@ -30,5 +38,27 @@ describe('fahrenheitToCelsius', () => {
     expect(() => fahrenheitToCelsius(null)).toThrow()
     expect(() => fahrenheitToCelsius(undefined)).toThrow()
     expect(() => fahrenheitToCelsius(true)).toThrow()
+  })
+})
+
+describe('convertDegrees', () => {
+  it('calls the correct functions in order', () => {
+    const fahrenheitToCelsiusSpy = jest.spyOn(
+      temperatureFuncs,
+      'fahrenheitToCelsius'
+    )
+    fahrenheitToCelsiusSpy.mockImplementation(() => 4)
+    const roundDecimalsSpy = jest.spyOn(temperatureFuncs, 'roundDecimals')
+
+    convertDegrees(0)
+
+    expect(fahrenheitToCelsiusSpy).toHaveBeenCalledTimes(1)
+    expect(fahrenheitToCelsiusSpy).toHaveBeenCalledWith(0)
+    expect(roundDecimalsSpy).toHaveBeenCalledTimes(1)
+    expect(roundDecimalsSpy).toHaveBeenCalledWith(4)
+
+    // undo mocking the module functions
+    fahrenheitToCelsiusSpy.mockRestore()
+    roundDecimalsSpy.mockRestore()
   })
 })
